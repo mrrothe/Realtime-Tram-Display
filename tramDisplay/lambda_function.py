c3c61ea831af55ea3d19be97d2b7c473ae80dac4
Martin Rothe <martin@rothe.uk>
2018-12-31 11:02:03 +0000

    Migrated CSS to grid to improve mobile display

--- tramDisplay/lambda_function.py
+++ tramDisplay/lambda_function.py
@@ -3,99 +3,98 @@ import json
 from string import Template
 import re
 html_template="""
-<!doctype html>
+<meta http-equiv="refresh" content="30">
 <html lang="en">
-<meta http-equiv="refresh" content="30" />
 <head>
-    <meta charset="utxf-8" />
-    <title>Tram Time Display</title>
-    <meta name="viewport" content="width=device-width, user-scalable=no">
-    <style>
-        @font-face {
-            font-family: 'LCDDot TR';
-            src: url('https://s3.rothe.uk/LCDDot-TR.woff2') format('woff2'),
-                url('https://s3.rothe.uk/LCDDot-TR.woff') format('woff');
-            font-weight: normal;
-            font-style: normal;
-        }
-        body {
-            background-color: slategrey;
-        }
-        #tramdisplay {
-            background-color: darkslategrey;
-            width: 90%;
-            padding: 30px;
-            margin: auto;
-            color: orange;
-            font-size: 6em;
-            font-family: 'LCDDot TR';
-        }
-        .tramvisit {
-            display: block;
-            width: 100%;
-        }
-        .tramdetail {
-            margin-bottom: 30px;
-            float: left;
-            display: block;
-            width: 33%;
-        }
-        .destination {
-            text-align: left
-        }
-        .time {
-            text-align: right
-        }
-        .late {
-            color: red;
-            text-align: center;
-        }
-        .ontime {
-            color: lime;
-            text-align: center;
-        }
-        #realtime {
-            color: #1E9CF1;
-            font-size: 2em;
-            margin: 30px;
-        }
-    </style>
+<meta charset="utf-8">
+<title>Tram Time Display</title>
+<meta name="viewport" content="width=device-width, user-scalable=no">
+<style>
+@font-face {
+    font-family: 'LCDDot TR';
+    src: url('https://s3.rothe.uk/LCDDot-TR.woff2') format('woff2'),
+        url('https://s3.rothe.uk/LCDDot-TR.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+}
+body{
+    background-color:darkslategrey;
+    
+}
+#tramdisplay{
+    width: 100%;
+    padding: 30px;
+    color: orange;
+    font-size:  8vw;
+    font-family: 'LCDDot TR';
+    display: grid;
+    grid-template-columns: 40% 25% 35%;
+    grid-column-gap: 3vw;
+    grid-row-gap: 2vw;
+}
+.destination {
+  grid-column-start: 1;
+  grid-column-end: 2 ;
+}
+.time {
+  grid-column-start: 3;
+  grid-column-end: 4;
+}
+.late {
+    grid-column-start: 2;
+    grid-column-end: 3;
+    color: red;
+}
+.ontime {
+    grid-column-start: 2;
+    grid-column-end: 3;
+    color: lime;
+}
+#realtime{
+    color: #1E9CF1;
+    font-size:  14vw;
+    grid-column-start: 1;
+    grid-column-end: 3;
+}
+</style>
 </head>
 <body>
-    <div id="tramdisplay">
+<div id="tramdisplay">
 """
-def response(message, status_code): #Formats response for API gateway
+def response(message, status_code):
     return {
         'statusCode': str(status_code),
         'body': message,
-        'headers': {'Content-Type': 'text/html',},
+        'headers': {
+            'Content-Type': 'text/html',
+            },
         }
 
 def lambda_handler(event, context):
-    out="" # Initialise return string
-    if "queryStringParameters" in event and "stop" in event['queryStringParameters']: # If stop paremter is defined
+    out=""
+    if "queryStringParameters" in event and "stop" in event['queryStringParameters']:
         stop=event['queryStringParameters']['stop']
     else:
         error="Please specify a stop"
         return response(error, 400)
-    stop = re.sub('\W+','',stop) # Strip non alphanumeric characters from search string
+    stop = re.sub('\W+','',stop)
+    print(stop)
     tram_data = requests.get("https://robinhood.arcticapi.com/network/stops/"+stop+"/visits").json()
     visits = tram_data["_embedded"]["timetable:visit"]
-    v=0 # Initialise counter
+    v=0
     for visit in visits:
-        if visit['isRealTime'] and v<5: # limit maximum trams to display on screen - adjust to match screensize
+        if visit['isRealTime'] and v<5:
             v+=1
-            if visit['expectedArrivalTime'] == visit['aimedArrivalTime']: # If tram is running to expected time
+            if visit['expectedArrivalTime'] == visit['aimedArrivalTime']:
                 ontime = "On Time"
                 ontimeclass="ontime"
             else:
                 ontime = "Late"
                 ontimeclass="late"
-            tramspan=Template("""<span class="tramvisit" id="tv_1">
+            tramspan=Template("""
             <span class="tramdetail destination">$dest</span>
             <span class="tramdetail $ontimeclass">$ontime</span>
             <span class="tramdetail time">$time</span>
-            </span>""")
+            """)
             out+=(tramspan.substitute(dest=visit['destinationName'],ontimeclass=ontimeclass,ontime=ontime,time=visit['displayTime']))
     return response(html_template+out+'<div id="realtime"> </div><script>function updateClock() {var now = new Date();document.getElementById("realtime").innerHTML = ((now.getHours()<10?"0":"")+now.getHours() + ":" + (now.getMinutes()<10?"0":"")+now.getMinutes()+ ":" + (now.getSeconds()<10?"0":"")+now.getSeconds());setTimeout(updateClock, 1000);};updateClock();</script></body></html>', 200)
-    
